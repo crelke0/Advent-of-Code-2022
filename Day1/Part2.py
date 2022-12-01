65c132f97cfb0532cdd1600ee0f4f59007f954c9
Clarke Elke <95830459+crelke0@users.noreply.github.com>
2022-12-01 10:44:21 -0600

    optimized

--- Day1/Part2.py
+++ Day1/Part2.py
@@ -1,17 +1,20 @@
 import os
 
-def func(log):
-  elves = []
-  elf = 0
-  for line in log:
+def func(lines):
+  n = 3
+  bests = [0]*n
+  total = 0
+  for line in lines:
     if line == '':
-      elves.append(elf)
-      elf = 0
+      for i in range(0, n - 1):
+        if total > bests[i]:
+          bests.insert(i, total)
+          bests = bests[:n]
+          break
+      total = 0
     else:
-      elf += int(line)
-  
-  elves = sorted(elves)
-  return elves[-1] + elves[-2] + elves[-3]
+      total += int(line)
+  return sum(bests)
 
 with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as input_file:
   puzzle_input = input_file.read()
