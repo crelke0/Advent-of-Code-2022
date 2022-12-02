ac77c7e5afd5cdcde01e7990031f01713387ced4
Clarke Elke <95830459+crelke0@users.noreply.github.com>
2022-12-02 08:42:52 -0600

    shortened part2.py

--- Day2/Part2.py
+++ Day2/Part2.py
@@ -5,8 +5,7 @@ with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as input_fi
 puzzle_input = puzzle_input.split('\n')
 score = 0
 for line in puzzle_input:
-  a, b = line.split(' ')
-  a = 'ABC'.index(a)
-  b = 'XYZ'.index(b)
+  a = 'ABC'.index(line[0])
+  b = 'XYZ'.index(line[2])
   score += b*3 + (b + a + 2)%3 + 1
 print(score)
