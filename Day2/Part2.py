0c61465a852740a6154a7d02d5bde8e77f6e7d84
crelke0 <clarke.elke.mail@gmail.com>
2022-12-01 23:14:30 -0600

    finished day2

---
+++ Day2/Part2.py
@@ -0,0 +1,16 @@
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as input_file:
+  puzzle_input = input_file.read()
+puzzle_input = puzzle_input.split('\n')
+score = 0
+for line in puzzle_input:
+  a, b = line.split(' ')
+  a = 'ABC'.index(a)
+  b = ('XYZ'.index(b) + a + 2)%3
+  score += b + 1
+  if a == b:
+    score += 3
+  elif a == (b + 2)%3:
+    score += 6
+print(score)
