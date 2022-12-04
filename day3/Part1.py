5d4af86661a6ba381eddc06c536292a79d4a4fc7
crelke0 <clarke.elke.mail@gmail.com>
2022-12-04 08:42:08 -0600

    changed '.intersection' to '&'

--- day3/Part1.py
+++ day3/Part1.py
@@ -6,7 +6,7 @@ lines = puzzle_input.split('\n')
 total = 0
 for line in lines:
   mid = len(line)//2
-  common = set(line[:mid]).intersection(line[mid:]).pop()
+  common = (set(line[:mid]) & set((line[mid:]))).pop()
   order = ord(common)
   priority = order - (38 if 64 < order < 91 else 96)
   total += priority
--- day3/Part2.py
+++ day3/Part2.py
@@ -5,7 +5,7 @@ with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as input_fi
 lines = puzzle_input.split('\n')
 total = 0
 for i in range(0, len(lines), 3):
-  common = set(lines[i]).intersection(lines[i + 1]).intersection(lines[i + 2]).pop()
+  common = (set(lines[i]) & set(lines[i + 1]) & set(lines[i + 2])).pop()
   order = ord(common)
   priority = order - (38 if 64 < order < 91 else 96)
   total += priority
